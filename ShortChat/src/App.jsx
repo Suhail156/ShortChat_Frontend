04ed181799086b21e99c531874cd0b070689362a
sanahiravp0 <sanahiravpo@gmail.com>
2025-04-08 00:18:48 +0530

    Initial commit or update code

--- ShortChat/src/App.jsx
+++ ShortChat/src/App.jsx
@@ -3,13 +3,11 @@ import { Routes ,Route} from 'react-router-dom'
 import Home from './Components/Home'
 import Login from './Components/Login'
 import Signup from './Components/Signup'
-import { Toaster } from 'react-hot-toast';
 
 const App = () => {
   return (
     <div>
       <Routes>
-      <Toaster position="top-right" reverseOrder={false} />
         <Route path='/home' element={<Home/>} ></Route>
         <Route path='/' element={<Login/>} ></Route>
         <Route path='/signup' element={<Signup/>} ></Route>
