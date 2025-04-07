fb1c8846d0d2bf625d948997116491981aa56b0c
sanahiravp0 <sanahiravpo@gmail.com>
2025-04-07 13:10:53 +0530

    Initial commit or update code

--- ShortChat/src/App.jsx
+++ ShortChat/src/App.jsx
@@ -7,8 +7,8 @@ const App = () => {
   return (
     <div>
       <Routes>
-        <Route path='/' element={<Home/>} ></Route>
-        <Route path='/login' element={<Login/>} ></Route>
+        <Route path='/home' element={<Home/>} ></Route>
+        <Route path='/' element={<Login/>} ></Route>
         <Route path='/signup' element={<Signup/>} ></Route>
       </Routes>
     </div>
