7df9e3f7dd31904d18dae288ddafaa15565fb32d
sanahiravp0 <sanahiravpo@gmail.com>
2025-04-07 13:01:56 +0530

    Initial commit or update code

--- ShortChat/src/api.jsx
+++ ShortChat/src/api.jsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3521",
+  // baseURL: "http://localhost:3521",
+  baseURL:"https://shortchat-backend.onrender.com"
 });
 
 export default api;
